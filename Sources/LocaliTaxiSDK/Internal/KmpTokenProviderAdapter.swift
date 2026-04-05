620a81fbd9f3c4dd508a8bb04ca929285dcab820
lukmanmakhaev <lukmanmakhaev@gmail.com>
2026-04-05 12:42:02 +0300

    авав

--- Sources/LocaliTaxiSDK/Internal/KmpTokenProviderAdapter.swift
+++ Sources/LocaliTaxiSDK/Internal/KmpTokenProviderAdapter.swift
@@ -1,11 +1,12 @@
 import Foundation
 import TaxiShared
 
-final class KmpTokenProviderAdapter: TokenProvider, Sendable {
+final class KmpTokenProviderAdapter: NSObject, TokenProvider {
     private let provider: TaxiAccessTokenProvider
 
     init(provider: TaxiAccessTokenProvider) {
         self.provider = provider
+        super.init()
     }
 
     func getAccessToken() -> String {
