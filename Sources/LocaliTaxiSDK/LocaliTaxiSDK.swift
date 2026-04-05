83216334b4fcfa549c477b81b8d02f5e22f4fab0
lukmanmakhaev <lukmanmakhaev@gmail.com>
2026-04-05 13:00:55 +0300

    пав

--- Sources/LocaliTaxiSDK/LocaliTaxiSDK.swift
+++ Sources/LocaliTaxiSDK/LocaliTaxiSDK.swift
@@ -1,8 +1,11 @@
 import TaxiShared
 
 public enum LocaliTaxiSDK {
+    nonisolated(unsafe) private static var tokenProviderAdapter: KmpTokenProviderAdapter?
+
     public static func initialize(accessTokenProvider: TaxiAccessTokenProvider) {
         let adapter = KmpTokenProviderAdapter(provider: accessTokenProvider)
+        tokenProviderAdapter = adapter
         TaxiSdk.companion.doInit(tokenProvider: adapter)
     }
 }
